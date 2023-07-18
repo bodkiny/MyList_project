425e246b0cb1f1fce9f54567770d9dadd2de6f22
Bohdan Rudenko <brudenko2804@gmail.com>
2023-07-18 14:16:27 +0300

    Add constructors to MyArrayList class

--- src/impl/MyArrayList.java
+++ src/impl/MyArrayList.java
@@ -3,6 +3,22 @@ package impl;
 import interfaces.MyList;
 
 public class MyArrayList<E> implements MyList<E> {
+    private static final int DEFAULT_CAPACITY = 15;
+    private Object[] container;
+    private int size;
+
+    public MyArrayList() {
+        container = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayList(final int initCapacity) {
+        if(initCapacity >= 0){
+            container = new Object[initCapacity];
+        } else {
+            throw new IllegalArgumentException("Invalid capacity value: " + initCapacity);
+        }
+    }
+
     @Override
     public int size() {
         return 0;
